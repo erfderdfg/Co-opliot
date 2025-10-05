31c7dea2abf22668eac5639b7d66d819dac30ea4
Molly Wu <99105007+erfderdfg@users.noreply.github.com>
2025-10-04 21:08:01 -0400

    Revert "Create Domain Object Relationship And Enum Relationship"
    This reverts commit d0a3f4ba235a8b5bf9a1dbc1d0b6665f8562352e.

--- app/src/main/java/com/app/co_opilot/domain/enums/RelationshipStatus.kt
+++ app/src/main/java/com/app/co_opilot/domain/enums/RelationshipStatus.kt
@@ -4,6 +4,4 @@ enum class RelationshipStatus {
     // Keeping it simple with only two fields for now
     LIKED_BY_USER1,
     LIKED_BY_USER2,
-    // MUTUAL, optional for both like each other
-    // NONE, no relation
 }
