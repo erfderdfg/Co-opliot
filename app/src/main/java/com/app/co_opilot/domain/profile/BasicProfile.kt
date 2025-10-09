46f9483f0dfbfba5b481da616eca032cc7b91bcb
Molly Wu <99105007+erfderdfg@users.noreply.github.com>
2025-10-09 14:39:28 -0400

    updated profile fiedls

--- app/src/main/java/com/app/co_opilot/domain/profile/BasicProfile.kt
+++ app/src/main/java/com/app/co_opilot/domain/profile/BasicProfile.kt
@@ -1,5 +1,11 @@
 package com.app.co_opilot.domain.profile
 
 // https://git.uwaterloo.ca/b5wu/co-opilot/-/work_items/20
-class BasicProfile {
-}
+
+enum class Gender { FEMALE, MALE, NON_BINARY, OTHER, UNSPECIFIED }
+data class BasicProfile(
+    val gender: Gender = Gender.UNSPECIFIED,
+    val religion: String? = null,
+    val nationality: String? = null,
+    val dateOfBirth: String? = null
+)
