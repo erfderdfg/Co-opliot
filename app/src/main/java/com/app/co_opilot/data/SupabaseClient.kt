d55d9a5989a1f0617eadd6a9cd4b74f84bc52602
Molly Wu <99105007+erfderdfg@users.noreply.github.com>
2025-10-29 12:33:39 -0400

    Add Supabase Provider

--- app/src/main/java/com/app/co_opilot/data/SupabaseClient.kt
+++ app/src/main/java/com/app/co_opilot/data/SupabaseClient.kt
@@ -1,13 +1,14 @@
 package com.app.co_opilot.data
 
 import com.app.co_opilot.BuildConfig
+import com.app.co_opilot.data.provider.SupabaseProvider
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 
-object SupabaseClient {
-    val client = createSupabaseClient(
+object SupabaseClient: SupabaseProvider {
+    override val client = createSupabaseClient(
         supabaseUrl = BuildConfig.SUPABASE_URL,
         supabaseKey = BuildConfig.SUPABASE_KEY
     ) {
---
+++ app/src/main/java/com/app/co_opilot/data/provider/SupabaseProvider.kt
@@ -0,0 +1,5 @@
+package com.app.co_opilot.data.provider
+
+interface SupabaseProvider {
+    val client: io.github.jan.supabase.SupabaseClient
+}
