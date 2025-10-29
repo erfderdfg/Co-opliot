293cd9bb7c3e14b65b514d4b3dc27df6b1b0ffdd
Molly Wu <99105007+erfderdfg@users.noreply.github.com>
2025-10-29 12:38:31 -0400

    Serialize Chat and Message Domain Objects

--- app/src/main/java/com/app/co_opilot/domain/Chat.kt
+++ app/src/main/java/com/app/co_opilot/domain/Chat.kt
@@ -1,7 +1,12 @@
 package com.app.co_opilot.domain
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Chat (
     val id: String,
-    val userOneId: String,
-    val userTwoId: String
+    @SerialName("user_one_id") val userOneId: String,
+    @SerialName("user_two_id")val userTwoId: String,
+    @SerialName("created_at") val createdAt: String
 )
--- app/src/main/java/com/app/co_opilot/domain/Message.kt
+++ app/src/main/java/com/app/co_opilot/domain/Message.kt
@@ -1,9 +1,13 @@
 package com.app.co_opilot.domain
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Message (
     val id: String,
-    val chatId: String,
-    val senderId: String,
+    @SerialName("chat_id") val chatId: String,
+    @SerialName("sender_id") val senderId: String,
     val message: String,
-    val sentAt: String
+    @SerialName("sent_at") val sentAt: String
 )
